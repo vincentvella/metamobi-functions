1697dddb6e7109a0269f45abb238be370c8a5d71
Vincent Vella <vincevella0@gmail.com>
2019-09-03 21:34:25 -0400

    Remove app directory and add hasura claims

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,26 @@
-import * as functions from 'firebase-functions';
+import * as admin from "firebase-admin";
+import * as functions from "firebase-functions";
+admin.initializeApp(functions.config().firebase);
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.processSignUp = functions.auth.user().onCreate(async user => {
+  console.log(user);
+  const customClaims = {
+    "https://hasura.io/jwt/claims": {
+      "x-hasura-default-role": "user",
+      "x-hasura-allowed-roles": ["user"],
+      "x-hasura-user-id": user.uid
+    }
+  };
+  // Set custom user claims on this newly created user.
+  try {
+    await admin.auth().setCustomUserClaims(user.uid, customClaims);
+    console.log("Custom claims added: ", customClaims);
+    // Update real-time database to notify client to force refresh.
+    const metadataRef = admin.database().ref("metadata/" + user.uid);
+    // Set the refresh time to the current UTC timestamp.
+    // This will be captured on the client to force a token refresh.
+    return metadataRef.set({ refreshTime: new Date().getTime() });
+  } catch (error) {
+    console.log(error);
+  }
+});
